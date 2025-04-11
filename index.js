06e1970784b3f845ebce95d110b47d826f4ee7bb
Jovanny "War" Berté <43629991+JovannyLB@users.noreply.github.com>
2025-04-11 15:41:09 -0300

    Abstracted most of the code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { readFileSync } from "fs";
 import axios from "axios";
+import * as utilities from "./utilities.js";
 
 const app = express();
 const port = 3000;
@@ -11,37 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const apiKey = JSON.parse(readFileSync("./apikey.json")).giantBombKey;
 
-const platform1Raw = axios.get(`https://www.giantbomb.com/api/platforms/?format=json&sort=name:asc&api_key=${apiKey}`);
-const platform2Raw = axios.get(`https://www.giantbomb.com/api/platforms/?format=json&offset=100&sort=name:asc&api_key=${apiKey}`);
-
 var platformId = 0;
 
 app.get("/", async (req, res) => {
   try {
-    const platforms1Response = await platform1Raw;
-    const platforms2Response = await platform2Raw;
-    const platforms = platforms1Response.data.results.concat(platforms2Response.data.results);
-
-    const numberOfGamesResponse = await axios.get(`https://www.giantbomb.com/api/games/?format=json&${platformId === 0 ? "" : "platforms=" + platformId + "&"}field_list=number_of_total_results&limit=1&api_key=${apiKey}`);
-    const randomGameId = Math.floor(Math.random() * (numberOfGamesResponse.data.number_of_total_results / 100));
-    const randomGameResponse = await axios.get(`https://www.giantbomb.com/api/games/?format=json&${platformId === 0 ? "" : "platforms=" + platformId + "&"}offset=${randomGameId}&limit=1&field_list=name,image,platforms,deck,original_release_date,site_detail_url&api_key=${apiKey}`);
-    const randomGame = randomGameResponse.data.results[0];
-
-    var randomGamePlatforms = "";
-
-    for (let i = 0; i < randomGame.platforms.length; i++) {
-      let currentString;
-      if (i === 0) {
-        currentString = `${randomGame.platforms[i].name}${randomGame.platforms.length === 1 ? "." : ""}`;
-      } else if (i === randomGame.platforms.length - 1) {
-        currentString = ` and ${randomGame.platforms[i].name}.`;
-      } else {
-        currentString = `, ${randomGame.platforms[i].name}`;
-      }
-      randomGamePlatforms += currentString;
-    }
-
-    console.log(randomGame.site_detail_url);
+    const platforms = await utilities.getAllPlatforms(apiKey);
+    const randomGame = await utilities.getRandomGame(apiKey, platformId);
+    const randomGamePlatforms = utilities.concatPlatformString(randomGame.platforms);
 
     res.render("index.ejs", { platforms: platforms, platformId: Number(platformId), gameName: randomGame.name, gameImage: randomGame.image.small_url, gamePlatforms: randomGamePlatforms, gameDeck: randomGame.deck, gameReleaseDate: randomGame.original_release_date, gameLink: randomGame.site_detail_url });
 
---
+++ utilities.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+/**
+ * Returns a string containing all the game's platforms formatted in a readble and grammarly correct manner.
+ * @param {[object]} platforms
+ * @returns Returns a complete string with corrected grammar containing all the game's platforms.
+ */
+export function concatPlatformString(platforms) {
+  let concatedString = "";
+
+  for (let i = 0; i < platforms.length; i++) {
+    let currentString;
+    if (i === 0) {
+      currentString = `${platforms[i].name}${platforms.length === 1 ? "." : ""}`;
+    } else if (i === platforms.length - 1) {
+      currentString = ` and ${platforms[i].name}.`;
+    } else {
+      currentString = `, ${platforms[i].name}`;
+    }
+    concatedString += currentString;
+  }
+
+  return concatedString;
+}
+
+/**
+ * Returns a complete list of all platforms and their respective ids.
+ * @param {string} apiKey
+ * @returns A concated array of objects containing platform names and ids.
+ */
+export async function getAllPlatforms(apiKey) {
+  let concatedArray = [];
+
+  const numberOfPlatforms = await axios.get(`https://www.giantbomb.com/api/platforms/?format=json&field_list=number_of_total_results&limit=1&api_key=${apiKey}`);
+
+  for (let i = 0; i <= Math.floor(numberOfPlatforms.data.number_of_total_results / 100); i++) {
+    const platformsRaw = await axios.get(`https://www.giantbomb.com/api/platforms/?format=json&offset=${100 * i}&field_list=name,id&&sort=name:asc&api_key=${apiKey}`);
+    concatedArray = concatedArray.concat(platformsRaw.data.results);
+  }
+
+  return concatedArray;
+}
+
+/**
+ * Returns a random game based on an specific platform (specified by the platformID) or a completely random game (if platformId is 0).
+ * @param {string} apiKey
+ * @param {number} platformId
+ * @returns A Random Game in an object
+ */
+export async function getRandomGame(apiKey, platformId) {
+  const numberOfGamesResponse = await axios.get(`https://www.giantbomb.com/api/games/?format=json&${platformId === 0 ? "" : "platforms=" + platformId + "&"}field_list=number_of_total_results&limit=1&api_key=${apiKey}`);
+  const randomGameId = Math.floor(Math.random() * Math.floor(numberOfGamesResponse.data.number_of_total_results / 100));
+  const randomGameResponse = await axios.get(`https://www.giantbomb.com/api/games/?format=json&${platformId === 0 ? "" : "platforms=" + platformId + "&"}offset=${randomGameId}&limit=1&field_list=name,image,platforms,deck,original_release_date,site_detail_url&api_key=${apiKey}`);
+  return randomGameResponse.data.results[0];
+}
